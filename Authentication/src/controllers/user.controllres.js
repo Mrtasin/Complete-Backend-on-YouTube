b02aba6904e914be5a8fe71788cdae68171be44d
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-07-05 01:25:35 +0530

    create authentication project and setup

---
+++ Authentication/src/controllers/user.controllres.js
@@ -0,0 +1,12 @@
+
+const registerUser = async (req, res) => {
+  const { email, name, passsword } = req.body;
+};
+
+const loginUser = async (req, res) => {
+  const { email, name, passsword } = req.body;
+};
+
+const logoutUser = async (req, res) => {
+  const { email, name, passsword } = req.body;
+};
