f9af8b34c7286feec22846d943c458c1adb8c84e
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-07-15 23:55:32 +0530

    feat: add multer middleware for file uploads and integrate cloudinary for image storage

---
+++ Student-Record/src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const fileUplodeOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    console.log(`response :- ${response}`);
+
+    return response;
+  } catch (error) {
+    console.error(`File uploding error :- ${error}`);
+    return null;
+  }
+};
+
+export default fileUplodeOnCloudinary;
