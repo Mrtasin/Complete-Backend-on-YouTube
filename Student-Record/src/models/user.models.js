d83fa7afec888a98494bdc1297fdd84b7bebb222
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-07-16 01:21:41 +0530

    feat(models): create user model with schema and role enums

---
+++ Student-Record/src/models/user.models.js
@@ -0,0 +1,58 @@
+import { model, Schema } from "mongoose";
+import { UserRoleEnum, UserRoleEnumArray } from "../utils/constent.js";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        loaclpath: String,
+      },
+      default: {
+        url: "https://placehold.co/600x400/orange/white",
+        loaclpath: "",
+      },
+    },
+    fullname: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: UserRoleEnumArray,
+      default: UserRoleEnum.USER,
+    },
+
+    verificationToken: String,
+    verificationExpiry: Date,
+    resetVerificationToken: String,
+    resetVerificationExpiry: Date,
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
---
+++ Student-Record/src/utils/constent.js
@@ -0,0 +1,13 @@
+export const UserRoleEnum = {
+  ADMIN: "admin",
+  USER: "user",
+};
+
+export const UserRoleEnumArray = Object.values(UserRoleEnum);
+
+export const StudentGenderEnum = {
+  MALE: "male",
+  FEMALE: "female",
+};
+
+export const StudentGenderEnumArray = Object.values(StudentGenderEnum);
