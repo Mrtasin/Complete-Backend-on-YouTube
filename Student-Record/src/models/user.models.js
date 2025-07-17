076376a8394d77cb0cd0277b22e36c79ef13aa1a
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-07-18 01:30:24 +0530

    feat(models): enhance user model with password hashing, token generation, and verification methods

--- Student-Record/src/models/user.models.js
+++ Student-Record/src/models/user.models.js
@@ -1,5 +1,8 @@
 import { model, Schema } from "mongoose";
 import { UserRoleEnum, UserRoleEnumArray } from "../utils/constent.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
   {
@@ -46,7 +49,7 @@ const userSchema = new Schema(
     },
 
     dpt: String,
-
+    refreshToken: String,
     verificationToken: String,
     verificationExpiry: Date,
     resetVerificationToken: String,
@@ -55,6 +58,47 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-const User = model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password"))
+    this.password = await bcrypt.hash(this.password, 10);
 
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+
+  this.refreshToken = token;
+
+  return token;
+};
+
+userSchema.methods.generateVerificationToken = function () {
+  const token = crypto.randomBytes(35).toString("hex");
+  this.verificationToken = token;
+  this.verificationExpiry = Date.now() + 10 * 60 * 1000;
+
+  return token;
+};
+
+const User = model("User", userSchema);
 export default User;
